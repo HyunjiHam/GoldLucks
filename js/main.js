9f770df776d27aaab044125b8c9ae157067c18c5
HyunjiHam <beautifuldaysh@gmail.com>
2016-09-10 13:36:24 +0900

    modified UI design & add chart

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/*window.onload = function() {
     // TODO:: Do your initialization job
 
     // add eventListener for tizenhwkey
@@ -18,4 +18,24 @@ window.onload = function() {
 
         contentText.innerHTML = (contentText.innerHTML === "Basic") ? "Tizen" : "Basic";
     });
+};*/
+
+window.onload = function(){
+	document.addEventListener('tizenhwkey',function(e){
+		var activePageId = $.mobile.activePage.attr('id');
+		if(e.keyName==='back'){
+				if(activePageId==='page1'||activePageId==='page2'){
+					$.mobile.changePage('#home');
+				}else{
+					history.back();
+				}
+		}
+	});
 };
+
+$('#fixed').on('pagebeforeshow',function beforeshow(){
+	var date = new Date(),
+		startTime = date.getHours()+':'+date.getMinutes();
+	alert('hi');
+	$('#startTime').val(startTime);
+});
