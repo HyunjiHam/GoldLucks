a92eee15f0f7dae8312836c5864d6ff4779bc0bc
HyunjiHam <beautifuldaysh@gmail.com>
2016-10-02 01:34:05 +0900

    Print money lists in main page 1. make the date order upside down 2. print money lists in main page using nested loop 3. add '.rds_delta' to .gitignore file

--- js/database.js
+++ js/database.js
@@ -1,28 +1,3 @@
-// var catId=1;
-// $("#cat1, #cat2, #cat3, #cat4, #cat5, #cat6").click(function(){
-//   catId = this.id;
-//   if (catId === "cat2")
-//     catId = 2;
-//   else if (catId === "cat3")
-//     catId = 3;
-//   else if (catId === "cat4")
-//     catId = 4;
-//   else if (catId === "cat5")
-//     catId = 5;
-//   else if (catId === "cat6")
-//     catId = 6;
-//   else
-//     catId = 1;
-// });
-//
-// var methodId = 1;
-// $("#radio1, #radio2").click(function(){
-//   methodId = this.id;
-//   if(methodId === "radio2")
-//     methodId = 2;
-//   else methodId = 1;
-// });
-
 function GoldLucksDB(){
 
 }
@@ -30,200 +5,138 @@ function GoldLucksDB(){
 (function strict(){
   GoldLucksDB.prototype={
 
-  init : function init(){
-    this.db={};
-    this.version = 1.0;
-    this.dbName="GoldLucksDB";
-    this.dbDisplayName="GoldLucksDisplay";
-    this.dbSize = 2*1024*1024;
-    this.moneys=[];
-    this.openDatabase();
-  },
-
-  openDatabase : function(){
-		if(window.openDatabase){
-			this.db = openDatabase(this.dbName, this.version, this.dbDisplayName, this.dbSize);
-			this.createTable(this.db);
-		}else {
-	        alert("Web SQL Database not supported");
-		}
-	},
-
-  dataView : function dataView(){
-    var db = this.db;
-    db.transaction(function(t) {
-			t.executeSql("SELECT * FROM money", [],
-				function(tran, r) {
-					for (var i = 0; i < r.rows.length; i++) {
-						console.log('dataView:'+i);
-            var row = r.rows.item(i);
-						var newEntryRow = $("#sampleList").clone();
-						newEntryRow.removeAttr("id");
-						newEntryRow.removeAttr("style");
-						newEntryRow.appendTo("ol");
-						newEntryRow.find(".showDate").text(row.date);
-						newEntryRow.find(".showAmount").text(row.amount);
-						newEntryRow.find(".showUsed").text(row.used);
-						newEntryRow.find(".showCat").text(row.category);
-						newEntryRow.find(".showMethod").text(row.method);
-						newEntryRow.find(".showIncome").text(row.income);
-						newEntryRow.find(".showMemo").text(row.memo);
-          }
-      }, function(t, e) {
-        alert("Error:" + e.message);
-        }
-    );
-  });
-},
-
-  insertData : function insertData(fromWhere,money){
-    var db = this.db;
-    if(fromWhere === "fromExpense"){
-			// var inputDate = $("#datepicker").val(),
-			// 	inputAmount = $("#eAmount").val(),
-			// 	inputused = $("#eUsed").val(),
-			// 	inputCategory = catId,
-			// 	inputMethod = methodId,
-			// 	inputMemo = $("#eMemo").val();
-
-			db.transaction(function(tx){
-				tx.executeSql(
-					"INSERT INTO money(date, amount, used, category, method, income, memo) VALUES (?,?,?,?,?,?,?);",
-					//[inputDate, inputAmount, inputused, inputCategory, inputMethod, 0, inputMemo],
-          [money.date,money.amount,money.used,money.category,money.method,0,money.memo],
-					function onSuccess() {//run if SQL succeeds
-            return;
-						//GoldLucksDB.dataView();
-					},
-					function onError(e) { //run if SQL fails
-						alert("Error:" + e.message);
-					}
-				);
-			});
-
-			// $("#datepicker").val("");
-			// $("#eAmount").val("");
-			// $("#eUsed").val("");
-			// catId=1;
-			// methodId=1;
-			// $("#eMemo").val("");
-		}
-
-		if(fromWhere === "fromIncome"){
-			// var inputDate = $("#datepicker2").val(),
-			// inputAmount = $("#iAmount").val(),
-			// inputused = $("#iUsed").val(),
-			// inputMemo = $("#iMemo").val();
-
-			db.transaction(function(tx){
-				tx.executeSql(
-					"INSERT INTO money(date, amount, used, income, memo) VALUES (?,?,?,?,?);",
-					[money.date, money.amount, money.used, 1, money.memo],
-					function onSuccess() {//run if SQL succeeds
-						this.dataView();
-					},
-					function onError(e) { //run if SQL fails
-						alert("Error:" + e.message);
-					}
-				);
-			});
-		}
-	},
-
-  createTable : function createTable(db){
-    db.transaction(function(tx) {
-            tx.executeSql("CREATE TABLE IF NOT EXISTS book"+
-            		"(bookName VARCHAR(20) PRIMARY KEY,"+
-            		"masterId VARCHAR(10) NOT NULL)");
-        });
-
-        db.transaction(function(tx) {
-            tx.executeSql("CREATE TABLE IF NOT EXISTS user"+
-            		"(userId VARCHAR(10) PRIMARY KEY," +
-            		"userPwd VARCHAR(10) )");
-        });
-
-        db.transaction(function(tx) {
-            tx.executeSql("CREATE TABLE IF NOT EXISTS fixedExpenses"+
-            		"(id INT(11) PRIMARY KEY,"+
-            		"amount FLOAT NOT NULL,"+
-            		"used VARCHAR(20) NOT NULL,"+
-            		"category INT(1) NOT NULL,"+
-            		"alarmDay INT(11) NOT NULL,"+
-            		"alarmHour INT(11) NOT NULL,"+
-            		"alarmMinute INT(11) NOT NULL,"+
-            		"memo VARCHAR(50) )" );
-        });
-
-        db.transaction(function(tx) {
-            tx.executeSql("CREATE TABLE IF NOT EXISTS money"+
-            		"(id INT PRIMARY KEY,"+
-            		"date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,"+
-            		"amount FLOAT NOT NULL,"+
-            		"used VARCHAR(30) NOT NULL,"+
-            		"category INT(1),"+
-            		"method INT(1),"+
-            		"income INT(1) NOT NULL,"+
-            		"memo VARCHAR(50) )" );
-        });
+    init : function init(){
+      this.db={};
+      this.version = 1.0;
+      this.dbName="GoldLucksDB";
+      this.dbDisplayName="GoldLucksDisplay";
+      this.dbSize = 2*1024*1024;
+      this.moneys=[];
+      this.openDatabase();
     },
 
+    openDatabase : function(){
+  		if(window.openDatabase){
+  			this.db = openDatabase(this.dbName, this.version, this.dbDisplayName, this.dbSize);
+  			this.createTable(this.db);
+  		}else {
+  	        alert("Web SQL Database not supported");
+  		}
+  	},
 
-    getMoney : function getMoney(firstDay,lastDay,printMoney,mainpage){
-      var money={};
+    insertData : function insertData(fromWhere,money){
       var db = this.db;
-      mainpage.moneys=[];
-      db.transaction(function (tx) {
-                tx.executeSql("SELECT * FROM money WHERE (date>=? AND date<=?)",[firstDay,lastDay],
-                function(tran,r){
-                    for(var i=0;i<r.rows.length;i++){
-                      var row = r.rows.item(i);
-                        var newEntryRow = $("#sampleList").clone();
-                        newEntryRow.removeAttr("id");
-                        newEntryRow.removeAttr("style");
-                        newEntryRow.appendTo("ol");
-                        newEntryRow.find(".showDate").text(row.date);
-                        newEntryRow.find(".showAmount").text(row.amount);
-                        newEntryRow.find(".showUsed").text(row.used);
-                        newEntryRow.find(".showCat").text(row.category);
-                        newEntryRow.find(".showMethod").text(row.method);
-                        newEntryRow.find(".showIncome").text(row.income);
-                        newEntryRow.find(".showMemo").text(row.memo);
-                    }
-										//console.log(r.rows.length);
-                    for(var i=0;i<r.rows.length;i++){
-                        var row = r.rows.item(i);
-	                      var date = row.date,
-	                              amount = row.amount,
-	                              used = row.used,
-	                              category = row.category,
-	                              method = row.method,
-	                              income = row.income,
-	                              memo = row.memo;
-                        money={
-                            'date': date,
-                            'amount': amount,
-                            'used' : used,
-                            'category' : category,
-                            'method' : method,
-                            'income' : income,
-                            'memo' : memo
-                        }
-												//console.log(date);
-                        mainpage.moneys.push(money);
-                        console.log(typeof(mainpage.moneys)+mainpage.moneys.length);
-                    }
-                    console.log('getMoney() in database is done');
-                    if(typeof(printMoney)==="function"){
-                      console.log(mainpage);
-                      printMoney.apply(mainpage,mainpage.moneys);
-                    }
-										//Mainpage.printMoney;
-                },function(tx,e){
-                    alert("Error:"+e.message);
-                }
-            );
-        });
-    }
-}
+      if(fromWhere === "fromExpense"){
+
+  			db.transaction(function(tx){
+  				tx.executeSql(
+  					"INSERT INTO money(date, amount, used, category, method, income, memo) VALUES (?,?,?,?,?,?,?);",
+            [money.date,money.amount,money.used,money.category,money.method,0,money.memo],
+  					function onSuccess() {//run if SQL succeeds
+              return;
+  					},
+  					function onError(e) { //run if SQL fails
+  						alert("Error:" + e.message);
+  					}
+  				);
+  			});
+
+  		}
+
+  		if(fromWhere === "fromIncome"){
+
+  			db.transaction(function(tx){
+  				tx.executeSql(
+  					"INSERT INTO money(date, amount, used, income, memo) VALUES (?,?,?,?,?);",
+  					[money.date, money.amount, money.used, 1, money.memo],
+  					function onSuccess() {//run if SQL succeeds
+  						return;
+  					},
+  					function onError(e) { //run if SQL fails
+  						alert("Error:" + e.message);
+  					}
+  				);
+  			});
+  		}
+  	},
+
+    createTable : function createTable(db){
+      db.transaction(function(tx) {
+              tx.executeSql("CREATE TABLE IF NOT EXISTS book"+
+              		"(bookName VARCHAR(20) PRIMARY KEY,"+
+              		"masterId VARCHAR(10) NOT NULL)");
+          });
+
+          db.transaction(function(tx) {
+              tx.executeSql("CREATE TABLE IF NOT EXISTS user"+
+              		"(userId VARCHAR(10) PRIMARY KEY," +
+              		"userPwd VARCHAR(10) )");
+          });
+
+          db.transaction(function(tx) {
+              tx.executeSql("CREATE TABLE IF NOT EXISTS fixedExpenses"+
+              		"(id INT(11) PRIMARY KEY,"+
+              		"amount FLOAT NOT NULL,"+
+              		"used VARCHAR(20) NOT NULL,"+
+              		"category INT(1) NOT NULL,"+
+              		"alarmDay INT(11) NOT NULL,"+
+              		"alarmHour INT(11) NOT NULL,"+
+              		"alarmMinute INT(11) NOT NULL,"+
+              		"memo VARCHAR(50) )" );
+          });
+
+          db.transaction(function(tx) {
+              tx.executeSql("CREATE TABLE IF NOT EXISTS money"+
+              		"(id INT PRIMARY KEY,"+
+              		"date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,"+
+              		"amount FLOAT NOT NULL,"+
+              		"used VARCHAR(30) NOT NULL,"+
+              		"category INT(1),"+
+              		"method INT(1),"+
+              		"income INT(1) NOT NULL,"+
+              		"memo VARCHAR(50) )" );
+          });
+      },
+
+
+      getMoney : function getMoney(firstDay,lastDay,printMoney,mainpage){
+        var money={};
+        var db = this.db;
+        mainpage.moneys=[];
+        db.transaction(function (tx) {
+                  tx.executeSql("SELECT * FROM money WHERE (date>=? AND date<=?)",[firstDay,lastDay],
+                  function(tran,r){
+                      for(var i=0;i<r.rows.length;i++){
+                          var row = r.rows.item(i);
+  	                      var date = row.date,
+  	                              amount = row.amount,
+  	                              used = row.used,
+  	                              category = row.category,
+  	                              method = row.method,
+  	                              income = row.income,
+  	                              memo = row.memo;
+                          money={
+                              'date': date,
+                              'amount': amount,
+                              'used' : used,
+                              'category' : category,
+                              'method' : method,
+                              'income' : income,
+                              'memo' : memo
+                          }
+                          mainpage.moneys.push(money);
+                          console.log(typeof(mainpage.moneys)+mainpage.moneys.length);
+                      }
+                      console.log('getMoney() in database is done');
+                      if(typeof(printMoney)==="function"){
+                        console.log(mainpage);
+                        printMoney.apply(mainpage,mainpage.moneys);
+                      }
+                  },function(tx,e){
+                      alert("Error:"+e.message);
+                  }
+              );
+          });
+      }
+  }
 }());
--- js/main.js
+++ js/main.js
@@ -10,10 +10,7 @@ Main.prototype={
     var db = this.db;
         this.compute();
         $('#mainMonth').html(this.year + '-' + this.month);
-        //this.getMoney();
-        //db.getMoney(this.firstDay,this.lastDay,this.printMoney);
         db.getMoney(this.firstDay,this.lastDay,this.printMoney,this);
-        //this.moneys = db.moneys;
   },
 
   compute : function Main_compute_date(){
@@ -25,44 +22,73 @@ Main.prototype={
           this.month='0'+this.month;
         this.firstDay = this.year+"-"+this.month+"-"+"01",
         this.lastDay = this.year+"-"+this.month+"-"+this.eDay;
+        this.today = new Date();
   },
 
   printMoney: function Main_print_money(moneys){
         var mainList = $('#cd-timeline');
         mainList.empty();
-        var moneyList=[];
-        moneyList=moneys;
-        var money;
-        if(moneyList!==undefined){
-          for (var j in moneyList){
-            money = moneyList[j];
-            console.log(money);
+        var time,
+            money,
+            check;
+        if(moneys!==undefined){
+          for (var i = this.eDay; i >= 0; i--) {
+            check=0;
+            for(var j in this.moneys){
+              money = this.moneys[j];
+              time=money.date.split('-');
+              if(parseInt(time[2])===i){
+                //this is the first time to insert money to i-th day
+                if(check===0){
+                  mainList.append('<div class="cd-timeline-block" id="block' + i + '"></div>');
+                  $('.cd-timeline-block#block' + i).append('<div class="cd-timeline-img" id="img' + i + '"></div>', '<div class="cd-timeline-content"id="content' + i + '"></div>');
+                  $('div[id="img' + i + '"]').append('<span class="cd-date">' + i + '</span>');
+                  $('.cd-timeline-content#content' + i).append('<ul data-role="listview" id="ul' + i + '"></ul>');
+                  check=1;
+                }
+                if(check===1){
+                  if(money.income===0){//in case of expense
+                    $('ul[id="ul' + i + '"]').append('<li>'+money.used+'<p class="ui-li-aside">'+money.amount+'(won)</p></li>');
+                    $('ul[id="ul' + i + '"]').listview().listview('refresh');
+                    console.log("find expense money in moneyList");
+                  }
+                  if(money.income===1){//in case of income
+                    $('ul[id="ul' + i + '"]').append('<li>'+money.used+'<p class="ui-li-aside">'+money.amount+'(won)</p></li>');
+                    $('ul[id="ul' + i + '"]').listview().listview('refresh');
+                    console.log("find income money in moneyList");
+                  }
+                }
+              }
+            }
           }
-          console.log("printMoney");
-          console.log(this.moneys.length);
-          /*moneys is undefined object..why..?
-          console.log(moneys.length);*/
         }else{
-            console.log('nothing in money DB yet');
-        }
-        //alert('Hi');
-        for (var i = 1; i < this.eDay+1; i++) {
-          mainList.append('<div class="cd-timeline-block" id="block' + i + '"></div>');
-          $('.cd-timeline-block#block' + i).append('<div class="cd-timeline-img" id="img' + i + '"></div>', '<div class="cd-timeline-content"id="content' + i + '"></div>');
-          $('div[id="img' + i + '"]').append('<span class="cd-date">' + i + '</span>');
-          $('.cd-timeline-content#content' + i).append('<ul data-role="listview" id="ul' + i + '"></ul>');
-          $('ul[id="ul' + i + '"]').append('<li>Food<p class="ui-li-aside">(-1200)(won)</p></li>');
-          $('ul[id="ul' + i + '"]').listview().listview('refresh');
+          var today = this.today.getDate();
+          mainList.append('<div class="cd-timeline-block" id="block' + today + '"></div>');
+          $('.cd-timeline-block#block' + today).append('<div class="cd-timeline-img" id="img' + today + '"></div>', '<div class="cd-timeline-content"id="content' + today + '"></div>');
+          $('div[id="img' + today + '"]').append('<span class="cd-date">' + today + '</span>');
+          $('.cd-timeline-content#content' + today).append('<ul data-role="listview" id="ul' + today + '"></ul>');
+          $('ul[id="ul' + today + '"]').append("<li><h1>Please insert today's money list!</h1><p class='ui-li-aside'></p></li>");
+          $('ul[id="ul' + today + '"]').listview().listview('refresh');
+          console.log('nothing in money DB yet');
         }
   },
-  getData : function get_input(){
+
+  getData : function get_input(fromWhere){
     var money={};
-    money.date = $("#datepicker").val();
-		money.amount = $("#eAmount").val();
-		money.used = $("#eUsed").val();
-		money.category = $("#select1>option:selected").val();
-		money.method = $("input:radio[name='radio1']:checked").val();
-		money.memo = $("#eMemo").val();
+    if(fromWhere==="fromExpense"){
+      money.date = $("#datepicker").val();
+  		money.amount = $("#eAmount").val();
+  		money.used = $("#eUsed").val();
+  		money.category = $("#select1>option:selected").val();
+  		money.method = $("input:radio[name='radio1']:checked").val();
+  		money.memo = $("#eMemo").val();
+    }
+    if(fromWhere==="fromIncome"){
+      money.date = $("#datepicker2").val();
+      money.amount = $("#iAmount").val();
+      money.used = $("#iUsed").val();
+      money.memo = $("#iMemo").val();
+    }
     return money;
   }
 }
@@ -73,18 +99,16 @@ Main.prototype={
 
   $("#addExpense").click(function(){
     var money = {};
-    money = mainpage.getData();
+    money = mainpage.getData("fromExpense");
     mainpage.db.insertData("fromExpense",money);
-    // GoldLucksDB.getMoney(Mainpage.firstDay,Mainpage.lastDay);
-    // Mainpage.printMoney;
     mainpage.init();
-    //mainpage.printMoney();
-    //getMoney(firstDay,lastDay);
-    //console.log(moneys.length);
   });
 
   $("#addIncome").click(function(){
-    mainpage.db.insertData("fromIncome");
+    var money = {};
+    money = mainpage.getData("fromIncome");
+    mainpage.db.insertData("fromIncome",money);
+    mainpage.init();
   });
 
 
